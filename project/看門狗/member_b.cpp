bca731ecfc06dc7e24668731148812a838d533e4
ChengHsunTai <137912642+ChengHsunTai@users.noreply.github.com>
2024-07-09 09:53:55 +0800

    Update member_b.cpp

--- project/看門狗/member_b.cpp
+++ project/看門狗/member_b.cpp
@@ -7,10 +7,12 @@
 
 class NodeB : public rclcpp::Node {
 public:
-    NodeB() : Node("node_b"), last_message_time_(this->now()) {
+    NodeB() : Node("node_b"), last_message_time_(this->now()), last_data_(2), count(0) {
         publisher_ = this->create_publisher<std_msgs::msg::Int32>("topic_b_to_a", 10);
         subscription_ = this->create_subscription<std_msgs::msg::Int32>(
             "topic_a_to_b", 10, std::bind(&NodeB::messageCallback, this, std::placeholders::_1));
+        timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(500), std::bind(&NodeB::PublishMessage, this));
         check_timer_ = this->create_wall_timer(
             std::chrono::milliseconds(1000), std::bind(&NodeB::checkForMessages, this));
     }
@@ -19,16 +21,31 @@ private:
 
     rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr subscription_;
+    rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::TimerBase::SharedPtr check_timer_;
     rclcpp::Time last_message_time_;
+    int last_data_;
+    int count;
+   
+    void PublishMessage (){
+    if (count % 2 == 1){
+    SendMessage(1);
+    }else {
+    SendMessage(0);
+    }
+    count++;
+    if (count == 10)
+    count = 0;
+    }
 
     void messageCallback(const std_msgs::msg::Int32::SharedPtr msg) {
         last_message_time_ = this->now();
-        int response = (msg->data == 1)? 0 : 1;
-        auto message = std_msgs::msg::Int32();
-        message.data = response;
-        RCLCPP_INFO(this->get_logger(), "Received %d, response %d", msg->data, message.data);
-        publisher_->publish(message);
+        if (last_data_ == msg->data){
+        RCLCPP_INFO(this->get_logger(), "repeatedly received %d .member_b may be crash!", msg->data);
+        }else{
+        RCLCPP_INFO(this->get_logger(), "Received %d", msg->data);
+        }
+    last_data_ = msg->data;
     }
 
     void checkForMessages() {
@@ -36,7 +53,12 @@ private:
             RCLCPP_WARN(this->get_logger(), "Warning: No messages received from node_a!");
         }
     }
-
+   
+    void SendMessage(int data){
+    auto message = std_msgs::msg::Int32();
+    message.data = data;
+    publisher_->publish(message);
+    }
 
 };
 
